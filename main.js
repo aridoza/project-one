133caa1167146b6e25497c923d090aa32da389d1
aridoza <aridoza85@gmail.com>
2016-03-02 17:33:48 -0500

    dice roll added

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-1. variables
-player1
-player2
-diceRollValue
+//variables
+player1;
+player2;
+diceRollValue;
 Player1Health = 30;
 Player2Health= 30;
 player1IsDead = false;
@@ -10,8 +10,8 @@ player2IsDead = false; //if one changes to true, game ends, alert("Player x got 
 
 
 
-2. game logic
-dice roll (6 sided die)
+//game logic
+//dice roll (6 sided die)
 
 //page loads, player1Health, player2Health both = 30
 //have a button under each player image to attack
@@ -19,21 +19,21 @@ dice roll (6 sided die)
 //only allow one player to attack at a time. First eventlistener is player1 click, then player2 click
 
 //kirby vs kermit: kirby always ready to attack, but kermit can be super smug img in beginning, then change to more worried when closer to death
-
-while player health is greater than Zero
-user clicks on button
-roll dice
-get dice value
-dice value = attack number (6 is a miss, dmg = 0)
-subtract attack number from player health
-if player health <= 0 at this point:
-playerisdead changed to true;
-alert "player dead"
-overwrite player image with dead image (skullface?)
-else, console.log player health remaining
-keep playing!
-
-repeat for player 2
+//
+// while player health is greater than Zero
+// user clicks on button
+// roll dice
+// get dice value
+// dice value = attack number (6 is a miss, dmg = 0)
+// subtract attack number from player health
+// if player health <= 0 at this point:
+// playerisdead changed to true;
+// alert "player dead"
+// overwrite player image with dead image (skullface?)
+// else, console.log player health remaining
+// keep playing!
+//
+// repeat for player 2
 
 
 
@@ -44,3 +44,46 @@ repeat for player 2
 //EXTRAS
 //add a character select screen to choose btwn different character images
 //add presidential candidates?
+
+
+//Diceroll function
+var diceValue;
+var diceRoll = function() {
+  var roll = Math.random();
+  var diceValue = 0;
+  if (roll <= 0.17) {
+    diceValue = 1;
+  } else if (roll <= 0.33) {
+    diceValue = 2;
+  } else if (roll <= 0.50) {
+    diceValue = 3;
+  } else if (roll <= 0.67) {
+    diceValue = 4;
+  } else if (roll <= 0.84) {
+    diceValue = 5;
+  } else if (roll <= 1) {
+    diceValue = 6;
+  }
+  console.log(diceValue);
+};
+
+player1AttackButton = document.querySelector('.AttackP1');
+player2AttackButton = document.querySelector('.AttackP2');
+player1AttackButton.addEventListener('click', function() {
+  alert("Kirby attacks Kermit!");
+});
+player2AttackButton.addEventListener('click', function() {
+  alert("Kermit attacks Kirby!");
+});
+
+
+
+//Game begins
+var count1 = 30;
+var count2 = 30;
+while (count1 > 0) {
+  player1AttackButton = document.getElementById('.AttackP1');
+  player1Click = player1AttackButton.addEventListener('click', function() {
+    alert("Hello!");
+  });
+}
---
+++ testsubjects/tests.js
@@ -0,0 +1,6 @@
+var button1 = document.querySelector('.Haha');
+console.log(button1);
+
+button1.addEventListener('click', function() {
+  alert("Why'd you click me?");
+})
