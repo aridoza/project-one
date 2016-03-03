c3b7f2e74aec2fbbdfcf30c248a3970ba9c1a723
aridoza <aridoza85@gmail.com>
2016-03-03 12:55:59 -0500

    moving reset button to top center page under title

--- main.js
+++ main.js
@@ -25,11 +25,11 @@
 // user clicks on button DONE (try to map to key presses instead of click)
 // roll dice - got into each attack function to return success rate of attack
 // get dice value - tied to attack
-// dice value = attack number (6 is a miss, dmg = 0) - 3 attacks, 
-// subtract attack number from player health
+// dice value = attack number (6 is a miss, dmg = 0) - 3 attacks,
+// subtract attack number from player health (copy/paste for P2, update names)
 // if player health <= 0 at this point:
 // playerisdead changed to true;
-// alert "player dead"
+// alert "player dead" - slapped silly
 // overwrite player image with dead image (skullface?)
 // else, console.log player health remaining
 // keep playing!
@@ -52,17 +52,28 @@ window.onload = function() {
   // player1;
   // player2;
   // diceRollValue;
-  var Player1Health = 30;
-  var Player2Health= 30;
-  var p1HealthDiv = document.querySelector('.P1Health');
-  p1HealthDiv.innerText = Player1Health;
-  var p2HealthDiv = document.querySelector('.P2Health');
-  p2HealthDiv.innerText = Player2Health;
+  var Player1Health = 100;
+  var Player2Health= 100;
+  var health1 = document.querySelector('.P1Health');
+  health1.innerText = Player1Health;
+  var health2 = document.querySelector('.P2Health');
+  health2.innerText = Player2Health;
   player1IsDead = false;
   player2IsDead = false; //if one changes to true, game ends, alert("Player x got slapped senseless!")
 
 
+///////getting player name////////////
+var name1 = document.querySelector('.P1N');
+var getName1 = prompt("Enter Player 1:");
+name1.innerHTML = ("Hands ready, " + getName1 + "!");
 
+var name2 = document.querySelector('.P2N');
+var getName2 = prompt("Enter Player 2:");
+name2.innerHTML = ("Hands steady, " + getName2 + "!");
+
+alert("Ready...");
+alert("FIGHT!!!!!!");
+///////////////////////////////////////
 
 
 
@@ -70,29 +81,208 @@ window.onload = function() {
 //Grab attack buttons & do stuff
 var player1LA = document.querySelector('.lightAttackP1'); //player1LA => player 1 light attack (higher hit chance)
 var player2LA = document.querySelector('.lightAttackP2');
+
+
+
+/////////////LIGHT ATTACKS ///////////////////////
+var player1LA = document.querySelector('.lightAttackP1'); //player1LA => player 1 light attack (higher hit chance)
+var player2LA = document.querySelector('.lightAttackP2');
+
+////////////////////P1 LIGHT ATTACK///////////////////////
 player1LA.addEventListener('click', function() {
-  alert("Kirby barely attacks Kermit!");
+  // alert("Kirby barely attacks Kermit!");
+  var health2 = document.querySelector('.P2Health');
+  // healthCounter = healthCounter - 3; //every time user clicks, health amt updates as 3 less; change -3 to -(somevariable from diceroll value)
+  // health.innerText = healthCounter;
+  var diceValue = Math.random();
+  if (diceValue <= 0.90) {
+    Player2Health = Player2Health - 5;
+    health2.innerText = Player2Health;
+  } else {
+    alert("Miss!")
+  }
+  if (Player2Health <= 0) { //endgame code, put into other 2 attack buttons
+  alert("Player 1 slapped em silly!");
+  alert("Game over!");
+  health2.innerText = "You cant HANDLE the slaps!";
+  var grabReset = document.querySelector('.Reset');
+  var resetButton = document.createElement('button'); //resetbutton code start
+  resetButton.setAttribute('type', 'button');
+  resetButton.innerText = 'New Slapfest?'
+  grabReset.appendChild(resetButton);
+  document.appendChild(grabReset);
+  resetButton.addEventListener('click', function() {
+    window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
+  })
+  var putButton = document.querySelector('.P2Health');
+  putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
+};
+  player1LA.removeEventListener('click', function(){
+    });
 });
+
+///////////////////P2 LIGHT ATTACK//////////////////////
 player2LA.addEventListener('click', function() {
-  alert("Kermit barely attacks Kirby!");
+  // alert("Kermit barely attacks Kirby!");
+  var health1 = document.querySelector('.P1Health');
+  // healthCounter = healthCounter - 3; //every time user clicks, health amt updates as 3 less; change -3 to -(somevariable from diceroll value)
+  // health.innerText = healthCounter;
+  var diceValue = Math.random();
+  if (diceValue <= 0.90) {
+    Player1Health = Player1Health - 5;
+    health1.innerText = Player1Health;
+  } else {
+    alert("Miss!")
+  }
+  if (Player1Health <= 0) { //endgame code, put into other 2 attack buttons
+  alert("Player 2 slapped em silly!");
+  alert("Game over!");
+  health1.innerText = "You cant HANDLE the slaps";
+  var resetButton = document.createElement('button'); //resetbutton code start
+  resetButton.setAttribute('type', 'button');
+  resetButton.innerText = 'New Slapfest?'
+  resetButton.addEventListener('click', function() {
+    window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
+  })
+  var putButton = document.querySelector('.P1Health');
+  putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
+};
+  player2LA.removeEventListener('click', function(){
+    });
 });
+
+
+
+
+///////NORMAL ATTACKS ///////////////////
 
 var player1MA = document.querySelector('.medAttackP1'); //MA = medium attack (avg hit chance)
 var player2MA = document.querySelector('.medAttackP2');
+
+////////////////////////P1 NORMAL ATTACK////////////////////
 player1MA.addEventListener('click', function() {
-  alert("Kirby mediumly attacks Kermit!");
-});
-player2MA.addEventListener('click', function() {
-  alert("Kermit mediumly attacks Kirby!");
+  // alert("Kirby mediumly attacks Kermit!");
+  var health2 = document.querySelector('.P2Health');
+  // healthCounter = healthCounter - 3; //every time user clicks, health amt updates as 3 less; change -3 to -(somevariable from diceroll value)
+  // health.innerText = healthCounter;
+  var diceValue = Math.random();
+  if (diceValue <= 0.70) {
+    Player2Health = Player2Health - 10;
+    health2.innerText = Player2Health;
+  } else {
+    alert("Miss!")
+  }
+  if (Player2Health <= 0) { //endgame code, put into other 2 attack buttons
+  alert("Player 1 slapped em silly!");
+  alert("Game over!");
+  health2.innerText = "You cant HANDLE the slaps";
+  var resetButton = document.createElement('button'); //resetbutton code start
+  resetButton.setAttribute('type', 'button');
+  resetButton.innerText = 'New Slapfest?'
+  resetButton.addEventListener('click', function() {
+    window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
+  })
+  var putButton = document.querySelector('.P2Health');
+  putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
+};
+  player1LA.removeEventListener('click', function(){
+    });
 });
 
+/////////////////////P2 NORMAL ATTACK///////////////////
+player2MA.addEventListener('click', function() {
+  // alert("Kermit mediumly attacks Kirby!");
+  var health1 = document.querySelector('.P1Health');
+  // healthCounter = healthCounter - 3; //every time user clicks, health amt updates as 3 less; change -3 to -(somevariable from diceroll value)
+  // health.innerText = healthCounter;
+  var diceValue = Math.random();
+  if (diceValue <= 0.70) {
+    Player1Health = Player1Health - 10;
+    health1.innerText = Player1Health;
+  } else {
+    alert("Miss!")
+  }
+  if (Player1Health <= 0) { //endgame code, put into other 2 attack buttons
+  alert("Player 2 slapped em silly!");
+  alert("Game over!");
+  health1.innerText = "You cant HANDLE the slaps";
+  var resetButton = document.createElement('button'); //resetbutton code start
+  resetButton.setAttribute('type', 'button');
+  resetButton.innerText = 'New Slapfest?'
+  resetButton.addEventListener('click', function() {
+    window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
+  })
+  var putButton = document.querySelector('.P1Health');
+  putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
+};
+  player2LA.removeEventListener('click', function(){
+    });
+});
+
+
+
+//////////////////STRONG ATTACKS //////////////////////
 var player1HA = document.querySelector('.strongAttackP1'); //heavy attack (has lower chance in dice roll. if roll less than certain value it misses)
 var player2HA = document.querySelector('.strongAttackP2');
+
+/////////////////P1 STRONG ATTACK////////////////////
 player1HA.addEventListener('click', function() {
-  alert("Kirby bitch slaps Kermit!");
+  // alert("Kirby bitch slaps Kermit!");
+  var health2 = document.querySelector('.P2Health');
+  // healthCounter = healthCounter - 3; //every time user clicks, health amt updates as 3 less; change -3 to -(somevariable from diceroll value)
+  // health.innerText = healthCounter;
+  var diceValue = Math.random();
+  if (diceValue >= 0.65) {
+    Player2Health = Player2Health - 20;
+    health2.innerText = Player2Health;
+  } else {
+    alert("Miss!")
+  }
+  if (Player2Health <= 0) { //endgame code, put into other 2 attack buttons
+  alert("Player 1 slapped em silly!");
+  alert("Game over!");
+  health2.innerText = "You cant HANDLE the slaps";
+  var resetButton = document.createElement('button'); //resetbutton code start
+  resetButton.setAttribute('type', 'button');
+  resetButton.innerText = 'New Slapfest?'
+  resetButton.addEventListener('click', function() {
+    window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
+  })
+  var putButton = document.querySelector('.P2Health');
+  putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
+};
+  player1LA.removeEventListener('click', function(){
+    });
 });
+
+////////////P2 STRONG ATTACK/////////////////////////
 player2HA.addEventListener('click', function() {
-  alert("Kermit bitch slaps Kirby!");
+  // alert("Kermit bitch slaps Kirby!");
+  var health1 = document.querySelector('.P1Health');
+  // healthCounter = healthCounter - 3; //every time user clicks, health amt updates as 3 less; change -3 to -(somevariable from diceroll value)
+  // health.innerText = healthCounter;
+  var diceValue = Math.random();
+  if (diceValue >= 0.65) {
+    Player1Health = Player1Health - 20;
+    health1.innerText = Player1Health;
+  } else {
+    alert("Miss!")
+  }
+  if (Player1Health <= 0) { //endgame code, put into other 2 attack buttons
+  alert("Player 2 slapped em silly!");
+  alert("Game over!");
+  health1.innerText = "You cant HANDLE the slaps";
+  var resetButton = document.createElement('button'); //resetbutton code start
+  resetButton.setAttribute('type', 'button');
+  resetButton.innerText = 'New Slapfest?'
+  resetButton.addEventListener('click', function() {
+    window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
+  })
+  var putButton = document.querySelector('.P1Health');
+  putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
+};
+  player2LA.removeEventListener('click', function(){
+    });
 });
 
 
--- testsubjects/tests.js
+++ testsubjects/tests.js
@@ -6,11 +6,21 @@ var healthCounter;
 
 window.onload = function() {
 
-var healthCounter = 100;
+  function playSound(file) {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', file);
+    audioElement.play();
+  }
+
+var health1Counter = 100;
+var health2Counter = 100;
+
 var button1 = document.querySelector('.Haha');
 console.log(button1);
-var health = document.querySelector('.health');
-health.innerText = healthCounter;
+var health1 = document.querySelector('.health1');
+health1.innerText = health1Counter;
+var health2 = document.querySelector('.health2');
+health2.innerText = health2Counter;
 
 
 // if (healthCounter >= 0) { //wrapped ALL 3 attack functions in this loop
@@ -18,27 +28,29 @@ health.innerText = healthCounter;
 //LIGHT ATTACK
 button1.addEventListener('click', function() {
   // alert("Why'd you click me?");
-  var health = document.querySelector('.health');
+
+
+  var health2 = document.querySelector('.health2');
   // healthCounter = healthCounter - 3; //every time user clicks, health amt updates as 3 less; change -3 to -(somevariable from diceroll value)
   // health.innerText = healthCounter;
   var diceValue = Math.random();
   if (diceValue <= 0.90) {
-    healthCounter = healthCounter - 5;
-    health.innerText = healthCounter;
+    health2Counter = health2Counter - 5;
+    health2.innerText = health2Counter;
   } else {
     alert("Miss!")
   }
-  if (healthCounter <= 0) { //endgame code, put into other 2 attack buttons
+  if (health2Counter <= 0) { //endgame code, put into other 2 attack buttons
   alert("Player 1 slapped em silly!");
   alert("Game over!");
-  health.innerText = "You cant HANDLE the slaps";
+  health2.innerText = "You cant HANDLE the slaps";
   var resetButton = document.createElement('button'); //resetbutton code start
   resetButton.setAttribute('type', 'button');
   resetButton.innerText = 'New Slapfest?'
   resetButton.addEventListener('click', function() {
     window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
   })
-  var putButton = document.querySelector('.health');
+  var putButton = document.querySelector('.health2');
   putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
 };
   button1.removeEventListener('click', function(){
@@ -53,25 +65,25 @@ console.log(button2);
 
 //NORMAL ATTACK
 button2.addEventListener('click', function() {
-  var health = document.querySelector('.health');
+  var health2 = document.querySelector('.health2');
   var diceValue = Math.random();
   if (diceValue <= 0.70) {
-    healthCounter = healthCounter - 10;
-    health.innerText = healthCounter;
+    health2Counter = health2Counter - 10;
+    health2.innerText = health2Counter;
   } else {
     alert("Miss!");
   }
-    if (healthCounter <= 0) { //endgame code, put into other 2 attack buttons
+    if (health2Counter <= 0) { //endgame code, put into other 2 attack buttons
     alert("Player 1 slapped em silly!");
     alert("Game over!");
-    health.innerText = "You cant HANDLE the slaps";
+    health2.innerText = "You cant HANDLE the slaps";
     var resetButton = document.createElement('button'); //resetbutton code start
     resetButton.setAttribute('type', 'button');
     resetButton.innerText = 'New Slapfest?'
     resetButton.addEventListener('click', function() {
       window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
     })
-    var putButton = document.querySelector('.health');
+    var putButton = document.querySelector('.health2');
     putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
   };
     button2.removeEventListener('click', function(){
@@ -85,25 +97,25 @@ console.log(button2);
 
 //STRONG ATTACK
 button3.addEventListener('click', function() {
-  var health = document.querySelector('.health');
+  var health2 = document.querySelector('.health2');
   var diceValue = Math.random();
   if (diceValue >= 0.65) {
-    healthCounter = healthCounter - 20;
-    health.innerText = healthCounter;
+    health2Counter = health2Counter - 20;
+    health2.innerText = health2Counter;
   } else {
     alert("Miss!");
   }
-  if (healthCounter <= 0) { //endgame code, put into other 2 attack buttons
+  if (health2Counter <= 0) { //endgame code, put into other 2 attack buttons
   alert("Player 1 slapped em silly!");
   alert("Game over!");
-  health.innerText = "You cant HANDLE the slaps";
+  health2.innerText = "You cant HANDLE the slaps";
   var resetButton = document.createElement('button'); //resetbutton code start
   resetButton.setAttribute('type', 'button');
   resetButton.innerText = 'New Slapfest?'
   resetButton.addEventListener('click', function() {
     window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
   })
-  var putButton = document.querySelector('.health');
+  var putButton = document.querySelector('.health2');
   putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
 };
   button3.removeEventListener('click', function(){
@@ -112,16 +124,133 @@ button3.addEventListener('click', function() {
 });
 
 
-//
-//  if (healthCounter <= 0) {
-//   alert("Player 1 slapped em silly!");
-//   return "Game Over";
-// } else {
-//
-// }
-//  //end of giant MAIN loop
-//
-// };
+///////END PLAYER 1 ATTACKS ///////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+
+
+/////////PLAYER 2 ATTACKS ////////////////
+
+
+var button4 = document.querySelector('.P2L');
+console.log(button1);
+var health1 = document.querySelector('.health1');
+health1.innerText = health1Counter;
+var health2 = document.querySelector('.health2');
+health2.innerText = health2Counter;
+
+
+// if (healthCounter >= 0) { //wrapped ALL 3 attack functions in this loop
+
+//LIGHT ATTACK
+button4.addEventListener('click', function() {
+  // alert("Why'd you click me?");
+  var health1 = document.querySelector('.health1');
+  // healthCounter = healthCounter - 3; //every time user clicks, health amt updates as 3 less; change -3 to -(somevariable from diceroll value)
+  // health.innerText = healthCounter;
+  var diceValue = Math.random();
+  if (diceValue <= 0.90) {
+    health1Counter = health1Counter - 5;
+    health1.innerText = health1Counter;
+  } else {
+    alert("Miss!")
+  }
+  if (health1Counter <= 0) { //endgame code, put into other 2 attack buttons
+  alert("Player 1 slapped em silly!");
+  alert("Game over!");
+  health1.innerText = "You cant HANDLE the slaps";
+  var resetButton = document.createElement('button'); //resetbutton code start
+  resetButton.setAttribute('type', 'button');
+  resetButton.innerText = 'New Slapfest?'
+  resetButton.addEventListener('click', function() {
+    window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
+  })
+  var putButton = document.querySelector('.health1');
+  putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
+};
+  button4.removeEventListener('click', function(){
+    });
+
+});
+
+//trying to tie heavy attack hit success to random number
+//Works! for med/light attacks: lower the >= diceValue for higher success rate
+var button5 = document.querySelector('.P2M');
+console.log(button2);
+
+//NORMAL ATTACK
+button5.addEventListener('click', function() {
+  //trying to add playSound
+    function playSound(file) {
+      var audioElement = document.createElement('audio');
+      audioElement.setAttribute('src', file);
+      audioElement.play('https://www.freesound.org/people/waterboy920/sounds/67034/');
+    }
+  //end trying to add playSound
+  var health1 = document.querySelector('.health1');
+  var diceValue = Math.random();
+  if (diceValue <= 0.70) {
+    health1Counter = health1Counter - 10;
+    health1.innerText = health1Counter;
+  } else {
+    alert("Miss!");
+  }
+    if (health1Counter <= 0) { //endgame code, put into other 2 attack buttons
+    alert("Player 2 slapped em silly!");
+    alert("Game over!");
+    health1.innerText = "You cant HANDLE the slaps";
+    var resetButton = document.createElement('button'); //resetbutton code start
+    resetButton.setAttribute('type', 'button');
+    resetButton.innerText = 'New Slapfest?'
+    resetButton.addEventListener('click', function() {
+      window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
+    })
+    var putButton = document.querySelector('.health1');
+    putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
+  };
+    button5.removeEventListener('click', function(){
+      });
+
+});
+
+//Same for normal attack
+var button6 = document.querySelector('.P2H');
+console.log(button2);
+
+//STRONG ATTACK
+button6.addEventListener('click', function() {
+  var health2 = document.querySelector('.health1');
+  var diceValue = Math.random();
+  if (diceValue >= 0.65) {
+    health1Counter = health1Counter - 20;
+    health1.innerText = health1Counter;
+  } else {
+    alert("Miss!");
+  }
+  if (health1Counter <= 0) { //endgame code, put into other 2 attack buttons
+  alert("Player 2 slapped em silly!");
+  alert("Game over!");
+  health1.innerText = "You cant HANDLE the slaps";
+  var resetButton = document.createElement('button'); //resetbutton code start
+  resetButton.setAttribute('type', 'button');
+  resetButton.innerText = 'New Slapfest?'
+  resetButton.addEventListener('click', function() {
+    window.location.reload(); //found at http://stackoverflow.com/questions/5294842/refresh-a-page-using-javascript-or-html
+  })
+  var putButton = document.querySelector('.health1');
+  putButton.appendChild(resetButton); //resetButton code ends; remove/recode if something breaks
+};
+  button6.removeEventListener('click', function(){
+    });
+
+});
+
+
+
+
 
 };
 
@@ -154,3 +283,12 @@ button3.addEventListener('click', function() {
 // };
 // button2.removeEventListener('click', function(){
 //   });
+
+
+//trying to add playSound
+  // function playSound(file) {
+  //   var audioElement = document.createElement('audio');
+  //   audioElement.setAttribute('src', file);
+  //   audioElement.play('https://www.freesound.org/people/waterboy920/sounds/67034/');
+  // }
+//end trying to add playSound
